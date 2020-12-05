6ab718977e142b46799dc5e8479afda8e95a3442
Seth Tisue <seth@tisue.net>
2020-12-05 10:54:07 -0800

    avoid using type parameters on Left or Right
    this future-proofs the code for scala/scala#9303,
    and IMO the new code is just as good anyway

--- core/shared/src/main/scala/scodec/bits/package.scala
+++ core/shared/src/main/scala/scodec/bits/package.scala
@@ -40,12 +40,12 @@ package object bits extends ScalaVersionSpecific {
   private[bits] implicit class EitherOps[L, R](val self: Either[L, R]) extends AnyVal {
     def map[R2](f: R => R2): Either[L, R2] = self match {
       case Right(r)      => Right(f(r))
-      case l: Left[L, R] => l.asInstanceOf[Either[L, R2]]
+      case l             => l.asInstanceOf[Either[L, R2]]
     }
 
     def flatMap[R2](f: R => Either[L, R2]): Either[L, R2] = self match {
       case Right(r)      => f(r)
-      case l: Left[L, R] => l.asInstanceOf[Either[L, R2]]
+      case l             => l.asInstanceOf[Either[L, R2]]
     }
 
     def toOption: Option[R] = self match {
